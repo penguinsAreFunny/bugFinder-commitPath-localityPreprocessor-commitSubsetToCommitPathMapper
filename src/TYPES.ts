3bfd7f612840a8d45d42b7926276f54c2abf12e9
Robert Klinger <robert@k1inger.de>
2021-09-06 17:12:27 +0200

    fix: Symbol.for not alway generating unique Symbol

--- src/TYPES.ts
+++ src/TYPES.ts
@@ -1,7 +1,7 @@
 export const BUGFINDER_COMMITPATH_LOCALITYPREPROCESSOR_COMMITSUBSETTOCOMMITPATHMAPPER_TYPES = {
-    db:                         Symbol.for("DB"),
-    fromID:                     Symbol.for("FromID"),
-    skip:                       Symbol.for("Skip"),
-    n:                          Symbol.for("n"),
-    commitToCommitPathMapper:   Symbol.for("CommitToCommitPathMapper")
+    db:                         Symbol("DB"),
+    fromID:                     Symbol("FromID"),
+    skip:                       Symbol("Skip"),
+    n:                          Symbol("n"),
+    commitToCommitPathMapper:   Symbol("CommitToCommitPathMapper")
 }
